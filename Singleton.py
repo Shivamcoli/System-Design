5a6c216aed150ee116bd703615f7f17aa849f51d
Shivamcoli <shivamkolii789@gmail.com>
2025-11-11 23:40:38 +0530

    new learning

--- Singleton.py
+++ Singleton.py
@@ -18,3 +18,24 @@ obj1.drive()
 obj1.stop()    
 obj2= Car("Honda")
 obj2.drive()
+
+class Bike:
+    _instance = None
+    
+    def __new__(cls,name):
+        if cls._instance is None:
+            cls._instance = super(Bike,cls).__new__(cls)
+            cls._instance.name=name
+        return cls._instance
+    
+    def ride(self):
+        print(f"{self.name} is riding.")    
+        
+    def park(self):
+        print(f"{self.name} is parked.")
+        
+obj3 = Bike("Yamaha")
+obj3.ride()
+obj3.park()
+obj4 = Bike("Ducati")
+obj4.ride()
